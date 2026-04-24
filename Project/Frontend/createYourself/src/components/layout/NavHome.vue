2a3b0a0092765600f04178083444e413d2b47230
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-04-24 18:15:29 +0200

    Update Nav width

--- Project/Frontend/createYourself/src/components/layout/NavHome.vue
+++ Project/Frontend/createYourself/src/components/layout/NavHome.vue
@@ -5,7 +5,7 @@ import Logo from "@/components/ui/Logo.vue";
 
 <template>
   <div class="z-999 fixed top-0 left-0 w-full bg-[var(--surface-color)] shadow-sm h-[65px] flex items-center justify-center">
-    <div class="flex items-center justify-between w-[1400px] px-4 py-2 h-full">
+    <div class="flex items-center justify-between w-[1200px] px-4 py-2 h-full">
       <!-- Logo-->
       <Logo link="/"></Logo>
 
