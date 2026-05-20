d2e287311f10813650b94d50607dd438cdbce939
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-20 15:19:07 +0200

    Create screenButton editor component

---
+++ Project/Frontend/createYourself/src/components/ui/editor/ScreenButton.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+  const props = defineProps<{
+    svg: string,
+    title: string,
+    active: boolean,
+  }>()
+</script>
+
+<template>
+  <button :class="props.active ? 'active' : '' " class="hover:bg-gray-50 flex justify-center items-center gap-2 px-2 py-1 rounded-md m-1">
+    <div class="flex justify-center items-center">
+      <i :class="props.svg"></i>
+    </div>
+    <span>{{ props.title }}</span>
+  </button>
+</template>
+
+<style scoped>
+  .active{
+    background-color: var(--text-color);
+    color: var(--text-color-white);
+  }
+</style>
