f2ba5b416c6d94dbdc673e717eea0da693f3e97d
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-20 15:17:50 +0200

    Create Block component for editor

---
+++ Project/Frontend/createYourself/src/components/ui/editor/Block.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+  const props = defineProps<{
+    svg: string,
+    title: string,
+    subtitle: string,
+  }>();
+</script>
+
+<template>
+  <div class="hover:border-[var(--primary-color)] cursor-grab select-none flex flex-col items-start justify-start px-3 py-3 border border-gray-200 rounded-lg">
+    <div class="mb-2 flex items-center justify-center w-[30px] h-[30px] bg-[var(--primary-color-light)] text-[var(--primary-color)] rounded-md">
+      <i :class="props.svg"></i>
+    </div>
+    <span class="font-semibold">{{ props.title }}</span>
+    <span class="font-light text-[var(--text-color-light)] text-wrap text-sm">{{ props.subtitle }}</span>
+  </div>
+</template>
+
