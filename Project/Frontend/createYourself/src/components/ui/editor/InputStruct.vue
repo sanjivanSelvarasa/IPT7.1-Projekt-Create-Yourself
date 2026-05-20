42702df0187f373ace6a05ecc87ba87a516069b6
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-20 15:18:41 +0200

    Create InputScruct editor component

---
+++ Project/Frontend/createYourself/src/components/ui/editor/InputStruct.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+  const props = defineProps<{
+    title: string,
+  }>()
+</script>
+
+<template>
+  <div class="flex flex-col items-start justify-center gap-1">
+    <label class="font-semibold text-md">{{ props.title }}</label>
+    <slot></slot>
+  </div>
+</template>
