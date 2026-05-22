b7b2654e3e2578699fd1c1ba85b82f2f04e69f7b
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-22 21:21:00 +0200

    Create Background component

---
+++ Project/Frontend/createYourself/src/components/layout/Background.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="overflow-x-hidden bg-[var(--background-color)] w-full min-h-[100vh] h-full mx-auto">
+    <slot></slot>
+  </div>
+</template>
