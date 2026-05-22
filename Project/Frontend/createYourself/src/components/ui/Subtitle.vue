5deaae54ef020d39bd5da586c88d0648d84edbed
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-22 21:23:32 +0200

    Create Subtitle component

---
+++ Project/Frontend/createYourself/src/components/ui/Subtitle.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+  import SvgStruct from "@/components/ui/SvgStruct.vue";
+
+  const props = defineProps<{
+    title: string,
+  }>()
+</script>
+
+<template>
+  <div class="flex items-center justify-start gap-2">
+    <SvgStruct class="w-[35px] h-[35px] text-[var(--primary-color)] bg-[var(--primary-color-light)] rounded-lg">
+      <slot></slot>
+    </SvgStruct>
+    <h3 class="font-semibold">{{ props.title }}</h3>
+  </div>
+</template>
