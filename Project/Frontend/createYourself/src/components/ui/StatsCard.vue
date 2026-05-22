87fd1303dfd93483dfeb33095c13aae4b6d7fb65
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-22 21:23:09 +0200

    Create StatsCard component

---
+++ Project/Frontend/createYourself/src/components/ui/StatsCard.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+  const props = defineProps<{
+    title: string,
+    value: string,
+  }>()
+</script>
+
+<template>
+  <div class="col-span-1 flex flex-col items-start justify-center px-4 py-3 border border-gray-200 bg-gray-50 rounded-xl">
+    <span class="font-semibold text-2xl">{{ props.value }}</span>
+    <span class="text-[var(--text-color-light)] text-sm font-semibold">{{ props.title }}</span>
+  </div>
+</template>
