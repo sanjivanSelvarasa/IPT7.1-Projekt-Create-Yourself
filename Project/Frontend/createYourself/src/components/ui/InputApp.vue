1065c1c5cfd82ec67d7437761344e6f8f8bd4a0d
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-22 21:22:51 +0200

    Create InputApp component

---
+++ Project/Frontend/createYourself/src/components/ui/InputApp.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+  const props = defineProps<{
+    name: string,
+    for: string,
+  }>()
+</script>
+
+<template>
+  <div class="flex flex-col items-start justify-center gap-1 w-full">
+    <label class="font-medium text-sm" :for="props.for">{{ props.name }}</label>
+    <slot></slot>
+  </div>
+</template>
