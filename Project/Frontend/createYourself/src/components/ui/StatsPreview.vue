ea606025583e73c84d57274524a3516a92f5c357
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-22 21:23:21 +0200

    Create StatsPreview component

---
+++ Project/Frontend/createYourself/src/components/ui/StatsPreview.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+  const props = defineProps<{
+    title: string,
+    value: string,
+  }>()
+</script>
+
+<template>
+  <div class="mt-5">
+    <div class="flex flex-col items-start justify-center gap-2 text-sm font-semibold">
+      <span class="text-[var(--text-color-light)] uppercase">{{ props.title }}</span>
+      <span>{{ props.value }}</span>
+    </div>
+  </div>
+</template>
