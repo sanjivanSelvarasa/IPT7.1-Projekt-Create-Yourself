c69d91d01d418f072196612c2eeb8289d6baff77
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-22 21:21:34 +0200

    Create MainContent component

---
+++ Project/Frontend/createYourself/src/components/layout/MainContent.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <main class="w-full flex flex-col items-start justify-center gap-10 mt-7 px-4 xl:mx-auto">
+    <slot></slot>
+  </main>
+</template>
