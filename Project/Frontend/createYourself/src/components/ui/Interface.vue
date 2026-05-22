a8ee3d64f40fd20f8cfca32b38a4b0e5201e8489
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-22 21:23:00 +0200

    Create Interface component

---
+++ Project/Frontend/createYourself/src/components/ui/Interface.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="w-full border border-gray-200 rounded-2xl overflow-hidden px-8 py-8 bg-[var(--surface-color)] shadow-lg">
+    <slot></slot>
+  </div>
+</template>
