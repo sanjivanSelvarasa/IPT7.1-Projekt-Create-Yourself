d0f8cbd1a402824de11b4c92f0da561622b33cee
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-22 21:32:45 +0200

    Create profileType.ts

---
+++ Project/Frontend/createYourself/src/types/profileType.ts
@@ -0,0 +1,12 @@
+export type ProfileType = {
+  id: number,
+  username: string,
+  email: string,
+  firstName: string,
+  lastName: string,
+  profileImg: string,
+  bio: string,
+  preferredLanguage: string,
+  createdAt: Date,
+  updatedAt: Date,
+}
