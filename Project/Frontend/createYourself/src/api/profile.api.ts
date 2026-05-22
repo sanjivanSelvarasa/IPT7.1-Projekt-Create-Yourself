ba991a393ea9bf38026fa28f0cf18196f15a7ef1
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-22 21:20:50 +0200

    Create profile.api add all crud api's

---
+++ Project/Frontend/createYourself/src/api/profile.api.ts
@@ -0,0 +1,75 @@
+import {apiFetch} from "@/api/api.ts";
+import type {ChangeProfileType} from "@/types/changeProfileType.ts";
+import type {PasswordChange} from "@/types/passwordChange.ts";
+import type {ProfileType} from "@/types/profileType.ts";
+import type {LanguageType} from "@/types/languageType.ts";
+
+export async function getProfileApi() : Promise<ProfileType> {
+  return await apiFetch('/account/profile', {
+    method: 'GET',
+    headers:{
+      'Authorization': `Bearer ${localStorage.getItem('token')}`,
+    }
+  })
+}
+
+export async function updateProfileApi(profile: ChangeProfileType) : Promise<ProfileType>{
+  return await apiFetch('/account/profile', {
+    method: 'PUT',
+    body: JSON.stringify({
+      first_name: profile.firstname,
+      last_name: profile.lastname,
+      username: profile.username,
+      email: profile.email,
+      bio: profile.bio,
+    }),
+    headers:{
+      'Authorization': `Bearer ${localStorage.getItem('token')}`,
+    }
+  })
+}
+
+export async function updateProfilePictureApi(){
+  return await apiFetch('/account/profile/picture', {
+    method: 'POST',
+    // TODO multipart/form-data, feld image hinzufügen
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem('token')}`,
+    }
+  })
+}
+
+export async function updateLanguageApi(language: string) : Promise<LanguageType>{
+  return await apiFetch('/account/language', {
+    method: 'PUT',
+    body: JSON.stringify({
+      language_code: language,
+    }),
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem('token')}`,
+    }
+  })
+}
+
+export async function updatePasswordApi(password: PasswordChange){
+  return await apiFetch('/account/password', {
+    method: 'PUT',
+    body: JSON.stringify({
+      current_password: password.currentPassword,
+      new_password: password.newPassword,
+      confirm_password: password.confirmPassword,
+    }),
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem('token')}`,
+    }
+  })
+}
+
+export async function deleteAccountApi(){
+  return await apiFetch('/account', {
+    method: 'DELETE',
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem('token')}`,
+    }
+  })
+}
