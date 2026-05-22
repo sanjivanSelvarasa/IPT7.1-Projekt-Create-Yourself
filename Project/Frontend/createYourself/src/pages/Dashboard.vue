79d63bca1d691ffd650b89e9e14172c5260f2c14
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-22 21:26:00 +0200

    Update Dashboard refactored code & implement language / filter / routes

--- Project/Frontend/createYourself/src/pages/Dashboard.vue
+++ Project/Frontend/createYourself/src/pages/Dashboard.vue
@@ -6,13 +6,20 @@ import {usePortfolioStore} from "@/stores/portfolioStore.ts";
 import {useI18n} from "vue-i18n";
 import {useAuthStore} from "@/stores/authStore.ts";
 import router from "@/router";
+import Background from "@/components/layout/Background.vue";
+import MainContent from "@/components/layout/MainContent.vue";
+import {useProfileStore} from "@/stores/profileStore.ts";
 
 const portfolioStore = usePortfolioStore();
+const profileStore = useProfileStore();
 
 onMounted(async () => {
   try{
     await portfolioStore.getPortfolio()
+    await profileStore.getProfile()
   }catch{}
+
+  locale.value = profileStore.profileData?.preferredLanguage ?? 'de'
 })
 
 const searchText = ref<string>("");
@@ -20,28 +27,30 @@ const noResults = computed(() => {
   return searchText.value.length > 0 && updatedPortfolios.value?.length === 0
 })
 
+const status = ref<string>('all');
+
 const updatedPortfolios = computed(() => {
-  return portfolioStore.portfolios?.filter(t => t.title.toLowerCase().includes(searchText.value.toLowerCase()) || t.description.toLowerCase().includes(searchText.value.toLowerCase()));
+  const filteredPortfolios = portfolioStore.portfolios?.filter(t => t.title.toLowerCase().includes(searchText.value.toLowerCase()) || t.description.toLowerCase().includes(searchText.value.toLowerCase()));
+
+  if(status.value === "all")
+    return filteredPortfolios
+  else if(status.value === "private")
+    return filteredPortfolios?.filter(t => t.visibility.toLowerCase() === "private");
+  else
+    return filteredPortfolios?.filter(t => t.visibility.toLowerCase() === "public");
 })
 
 
 // language
-const { t } = useI18n();
+const { t, locale } = useI18n();
 
 const tl = (key: string) => t(`dashboard.${key}`);
-
-// logout
-const authStore = useAuthStore();
-async function onLogout() {
-  await authStore.logout();
-  await router.push("/login");
-}
 </script>
 
 <template>
-  <div class="overflow-x-hidden bg-[var(--background-color)] w-full min-h-[100vh] h-full absolute top-0 left-0">
+  <Background>
     <div class="absolute w-full h-full grid-bg -z-99"></div>
-    <NavApp @logout="onLogout"></NavApp>
+    <NavApp></NavApp>
     <header class="w-full flex items-center justify-between gap-5 mt-40 max-w-[1200px] xl:mx-auto px-5">
       <div class="flex flex-col items-start justify-between gap-2">
         <span class="uppercase text-[var(--primary-color)]">{{ tl("start.Preview") }}</span>
@@ -50,16 +59,16 @@ async function onLogout() {
       </div>
 
       <div>
-        <button class="sm:flex hidden text-nowrap hover:scale-101 hover:-translate-y-[1px] transition duration-100 items-center justify-center gap-2 px-4 py-2 bg-[var(--primary-color)] rounded-lg shadow-lg text-[var(--text-color-white)] cursor-pointer">
+        <RouterLink to="/create" class="sm:flex hidden text-nowrap hover:scale-101 hover:-translate-y-[1px] transition duration-100 items-center justify-center gap-2 px-4 py-2 bg-[var(--primary-color)] rounded-lg shadow-lg text-[var(--text-color-white)] cursor-pointer">
           <div class="flex items-center justify-center">
             <i class="fa-solid fa-plus"></i>
           </div>
           <span>{{ tl("start.create-button") }}</span>
-        </button>
+        </RouterLink>
       </div>
     </header>
 
-    <main class="w-full flex flex-col items-start justify-center gap-10 mt-10 max-w-[1200px] xl:mx-auto px-5">
+    <MainContent>
       <div class="flex-wrap flex items-center justify-start gap-5 w-full">
         <div class="flex items-start justify-between gap-2">
           <span class="flex items-center justify-center gap-2 px-4 py-2 rounded-full bg-[var(--surface-color)] shadow-lg">
@@ -88,11 +97,11 @@ async function onLogout() {
           <div class="text-[var(--text-color-light)] flex items-center justify-center">
             <i class="fa-solid fa-magnifying-glass"></i>
           </div>
-          <input v-model="searchText" class="outline-none w-full" type="search" :placeholder=" tl('start.bars-and-sort-functions.search-bar') " />
+          <input v-model="searchText" class="rounded-none! outline-none w-full" type="search" :placeholder=" tl('start.bars-and-sort-functions.search-bar') " />
         </div>
 
         <div class="md:flex hidden">
-          <select class="hover:border-[var(--primary-color)] hover:text-[var(--primary-color)] shadow-lg px-4 py-2 text-[var(--text-color-light)] outline-none border border-gray-200 rounded-lg bg-[var(--surface-color)] cursor-pointer" name="cars" id="cars">
+          <select v-model="status" class="hover:border-[var(--primary-color)] hover:text-[var(--primary-color)] shadow-lg px-4 py-2 text-[var(--text-color-light)] outline-none border border-gray-200 rounded-lg bg-[var(--surface-color)] cursor-pointer" name="cars" id="cars">
             <option value="all">{{ tl("start.bars-and-sort-functions.status-sorter-standard") }}</option>
             <option value="public">{{ tl("start.bars-and-sort-functions.status-sorter-published") }}</option>
             <option value="private">{{ tl("start.bars-and-sort-functions.status-sorter-draft") }}</option>
@@ -111,15 +120,15 @@ async function onLogout() {
         <CardDashboard v-for="portfolio in updatedPortfolios" :key="portfolio.id" :portfolio="portfolio"></CardDashboard>
 
         <!-- create project-->
-        <div v-if="!noResults" class="select-none group hover:border-[var(--primary-color)] cursor-pointer transition duration-150 relative bg-transparent w-full h-[350px] aspect-square rounded-2xl overflow-hidden border-3 border-gray-200 border-dashed">
+        <RouterLink to="/create" v-if="!noResults" class="select-none group hover:border-[var(--primary-color)] cursor-pointer transition duration-150 relative bg-transparent w-full h-[350px] aspect-square rounded-2xl overflow-hidden border-3 border-gray-200 border-dashed">
           <div class="flex flex-col items-center justify-center h-full w-full gap-3">
             <div class="group-hover:border-[var(--primary-color)] group-hover:text-[var(--primary-color)] transition duration-150 flex items-center justify-center w-[45px] h-[45px] border border-gray-200 rounded-lg text-[var(--text-color-light)]">
               <i class="fa-solid fa-plus"></i>
             </div>
-            <span class="font-semibold text-gray-600">{{ tl("start.new-card.description") }}</span>
-            <span class="text-[var(--text-color-light)]">{{ tl("start.new-card.click-effect") }}</span>
+            <span class="font-semibold text-gray-600 group-hover:text-[var(--primary-color)]">{{ tl("start.new-card.description") }}</span>
+            <span class="text-[var(--text-color-light)] group-hover:text-[var(--primary-color)]">{{ tl("start.new-card.click-effect") }}</span>
           </div>
-        </div>
+        </RouterLink>
 
         <div v-if="noResults" class="w-full h-full flex items-center justify-center">
           <div class="flex items-center justify-center gap-4 px-8 py-4 shadow-lg bg-[var(--surface-color)] text-[var(--text-color)] h-fit w-fit rounded-xl">
@@ -131,6 +140,6 @@ async function onLogout() {
         </div>
 
       </div>
-    </main>
-  </div>
+    </MainContent>
+  </Background>
 </template>
