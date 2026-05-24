3802913a659878de31e7cd54927c117a5fcfa407
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-25 00:09:51 +0200

    Create portfolioSection api add crud endpoints

---
+++ Project/Frontend/createYourself/src/api/portfolioSection.api.ts
@@ -0,0 +1,50 @@
+import {apiFetch} from "@/api/api.ts";
+import type {SectionType} from "@/types/sectionType.ts";
+
+export async function getSectionsApi(portfolioId: number, versionId: number) {
+  return await apiFetch(`/portfolio/${portfolioId}/versions/${versionId}/sections`, {
+    method: 'GET',
+    headers: {
+      'Authorization': `BEARER ${localStorage.getItem('token')}`,
+    }
+  })
+}
+
+export async function createSectionApi(portfolioId: number, versionId: number, section: SectionType) {
+  return await apiFetch(`/portfolio/${portfolioId}/versions/${versionId}/sections`, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem('token')}`,
+    },
+    body: JSON.stringify({
+      sectionType: section.sectionType,
+      title: section.title,
+      sortOrder: section.sortOrder,
+      isVisible: section.isVisible,
+    })
+  })
+}
+
+export async function updateSectionApi(portfolioId: number, versionId: number, sectionId: number, section: SectionType) {
+  return await apiFetch(`/portfolio/${portfolioId}/versions/${versionId}/sections/${sectionId}`, {
+    method: 'PUT',
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem('token')}`,
+    },
+    body: JSON.stringify({
+      sectionType: section.sectionType,
+      title: section.title,
+      sortOrder: section.sortOrder,
+      isVisible: section.isVisible,
+    })
+  })
+}
+
+export async function deleteSectionApi(portfolioId: number, versionId: number, sectionId: number) {
+  return await apiFetch(`/portfolio/${portfolioId}/versions/${versionId}/sections/${sectionId}`, {
+    method: 'DELETE',
+    headers: {
+      'Authorization': `Bearer ${localStorage.getItem('token')}`,
+    }
+  })
+}
