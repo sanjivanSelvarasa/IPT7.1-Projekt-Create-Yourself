d77f49e117148b40caf614eeed2167fb7896e150
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-26 22:30:55 +0200

    Create skillType.ts

---
+++ Project/Frontend/createYourself/src/types/skillType.ts
@@ -0,0 +1,10 @@
+export type SkillType = {
+  createdAt: Date,
+  description: string,
+  id: number,
+  level: number,
+  name: string,
+  sortOrder: number,
+  portfolioId: number,
+  skillId: number,
+}
