a7cf3959e8a3e3354275b77f15785ad78dda0fe3
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-26 22:30:09 +0200

    Create educationType.ts

---
+++ Project/Frontend/createYourself/src/types/educationType.ts
@@ -0,0 +1,11 @@
+export type EducationType = {
+  institutionName: string,
+  degree: string,
+  fieldOfStudy: string,
+  sortOrder: Number,
+  startDate: string,
+  endDate: string,
+  createdAt: Date,
+  id: number,
+  portfolioId: number,
+}
