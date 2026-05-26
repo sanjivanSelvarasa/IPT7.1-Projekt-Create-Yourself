e0eec495c4eb0814bf1e48fafe52439b2da529be
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-26 22:30:12 +0200

    Create experienceType.ts

---
+++ Project/Frontend/createYourself/src/types/experienceType.ts
@@ -0,0 +1,11 @@
+export type ExperienceType = {
+  companyName: string,
+  position: string,
+  description: string,
+  sortOrder: number,
+  startDate: Date,
+  endDate: Date,
+  createdAt: Date,
+  id: number,
+  portfolioId: number,
+}
