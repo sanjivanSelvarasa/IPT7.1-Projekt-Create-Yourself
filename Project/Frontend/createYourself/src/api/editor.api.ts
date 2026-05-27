e387bbc88c88362522e8fed68e660414b85a42b3
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-28 00:13:04 +0200

    Update editor.api.ts update output

--- Project/Frontend/createYourself/src/api/editor.api.ts
+++ Project/Frontend/createYourself/src/api/editor.api.ts
@@ -1,7 +1,9 @@
 import {apiFetch} from "@/api/api.ts";
 import type {CreateEditorBlockType} from "@/types/createEditorBlockType.ts";
+import type {EditorBlockType} from "@/types/editorBlockType.ts";
+import type {CreateTextEditorBlockType} from "@/types/createTextEditorBlockType.ts";
 
-export async function getEditorBlockApi(portfolioId: number, portfolioVersionId: number, sectionId: number) : Promise<CreateEditorBlockType[]> {
+export async function getEditorBlockApi(portfolioId: number, portfolioVersionId: number, sectionId: number) : Promise<EditorBlockType[]> {
   return await apiFetch(`/portfolio/${portfolioId}/versions/${portfolioVersionId}/sections/${sectionId}/blocks`, {
     method: 'GET',
     headers: {
@@ -10,7 +12,7 @@ export async function getEditorBlockApi(portfolioId: number, portfolioVersionId:
   })
 }
 
-export async function createEditorBlockApi(portfolioId: number, portfolioVersionId: number, sectionId: number, editorBlock: CreateEditorBlockType) {
+export async function createEditorBlockApi(portfolioId: number, portfolioVersionId: number, sectionId: number, editorBlock: CreateEditorBlockType | CreateTextEditorBlockType) : Promise<EditorBlockType> {
   return await apiFetch(`/portfolio/${portfolioId}/versions/${portfolioVersionId}/sections/${sectionId}/blocks`, {
     method: 'POST',
     headers: {
@@ -24,7 +26,7 @@ export async function createEditorBlockApi(portfolioId: number, portfolioVersion
   })
 }
 
-export async function updateEditorBlockApi(portfolioId: number, portfolioVersionId: number, sectionId: number, blockId: number, editorBlock: CreateEditorBlockType) {
+export async function updateEditorBlockApi(portfolioId: number, portfolioVersionId: number, sectionId: number, blockId: number, editorBlock: CreateEditorBlockType) : Promise<EditorBlockType> {
   return await apiFetch(`/portfolio/${portfolioId}/versions/${portfolioVersionId}/sections/${sectionId}/blocks/${blockId}`, {
     method: 'PUT',
     headers: {
