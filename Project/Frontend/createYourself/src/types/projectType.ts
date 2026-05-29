bcf2d92f6b97f3e25a1c746caae6dc26b3cd2123
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-29 23:59:15 +0200

    Create projectType.ts

---
+++ Project/Frontend/createYourself/src/types/projectType.ts
@@ -0,0 +1,13 @@
+export type ProjectType = {
+  title: string,
+  description: string,
+  imageUrl?: string,
+  projectUrl: string,
+  githubUrl: string,
+  sortOrder: number,
+  startDate?: Date,
+  endDate?: Date,
+  createdAt: Date,
+  updatedAt: Date,
+  id: number,
+}
