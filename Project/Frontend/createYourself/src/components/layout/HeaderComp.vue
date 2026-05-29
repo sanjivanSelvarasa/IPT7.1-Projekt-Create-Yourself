60a0ff74466f0dfe21913ab31e055a7e2f3bf94d
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-29 23:54:18 +0200

    Update HeaderComp improved responsibilities

--- Project/Frontend/createYourself/src/components/layout/HeaderComp.vue
+++ Project/Frontend/createYourself/src/components/layout/HeaderComp.vue
@@ -7,11 +7,11 @@
 </script>
 
 <template>
-  <header class="w-full flex items-center justify-between gap-5 mt-40 max-w-[1200px] xl:mx-auto px-5">
-    <div class="flex flex-col items-start justify-between gap-2">
+  <header class="w-full mt-25 sm:mt-40 max-w-[1200px] xl:mx-auto px-5 text-center sm:text-left">
+    <div class="flex flex-col items-center sm:items-start justify-between gap-2">
       <span class="uppercase text-[var(--primary-color)]">{{ props.tag }}</span>
       <h1>{{ props.title }}</h1>
-      <span class="text-[var(--text-color-light)] text-wrap max-w-[750px]">{{ props.subtitle }}</span>
+      <span class="text-[var(--text-color-light)] text-wrap max-w-[750px] text-center sm:text-left">{{ props.subtitle }}</span>
     </div>
   </header>
 </template>
