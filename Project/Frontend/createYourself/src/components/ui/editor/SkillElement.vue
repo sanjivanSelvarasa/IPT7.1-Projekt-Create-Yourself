1a6042105005bcb9a8b91900cacae1a8358f6dcc
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-29 23:55:27 +0200

    Update SkillElement improved UI text view

--- Project/Frontend/createYourself/src/components/ui/editor/SkillElement.vue
+++ Project/Frontend/createYourself/src/components/ui/editor/SkillElement.vue
@@ -1,6 +1,4 @@
 <script lang="ts" setup>
-  import {computed} from "vue";
-
   const props = defineProps<{
     name: string,
     level: number,
@@ -10,8 +8,8 @@
 <template>
   <div class="w-full py-3">
     <div class="flex items-center justify-between">
-      <span class="font-semibold text-sm">{{name}}</span>
-      <span class="text-xs text-[var(--text-color-light)]">{{level}} %</span>
+      <span class="font-semibold text-sm">{{name.slice(0, 30)}}</span>
+      <span class="text-xs text-[var(--text-color-light)] text-nowrap">{{level}} %</span>
     </div>
     <div class="relative my-1 w-full h-2 rounded-full bg-gray-200 overflow-hidden">
       <div :style="{ width:`${props.level}%`}" class="rounded-full h-full absolute bg-linear-to-r from-[var(--primary-color)] to-[var(--secondary-color)]"></div>
