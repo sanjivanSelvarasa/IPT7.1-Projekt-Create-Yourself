0872ee26806d8acf3233f3f3b5328f3f60bdba42
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-29 23:55:33 +0200

    Create SocialLinkModul.vue

---
+++ Project/Frontend/createYourself/src/components/ui/editor/SocialLinkModul.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+
+import ModulStruct from "@/components/ui/editor/ModulStruct.vue";
+</script>
+
+<template>
+  <ModulStruct type="Link" name="5 Einträge" svg="fa-solid fa-link">
+    <slot></slot>
+  </ModulStruct>
+</template>
