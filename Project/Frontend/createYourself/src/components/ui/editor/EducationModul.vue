322cf690f7c77912d4ab55a3b6b30e522e912074
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-05-29 23:54:30 +0200

    Create EducationModul.vue

---
+++ Project/Frontend/createYourself/src/components/ui/editor/EducationModul.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+
+import ModulStruct from "@/components/ui/editor/ModulStruct.vue";
+</script>
+
+<template>
+  <ModulStruct type="Ausbildung" name="3 Einträge" svg="fa-solid fa-graduation-cap">
+    <div class="flex flex-col gap-2">
+      <slot></slot>
+    </div>
+  </ModulStruct>
+</template>
