3901d14531b68908c2f42ee1e486b93fce97febe
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-06-03 11:48:19 +0200

    Create AddButton component for editor

---
+++ Project/Frontend/createYourself/src/components/ui/editor/AddButton.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <button class="hover:border-[var(--primary-color)] hover:text-[var(--primary-color)] transition duration-100 outline-none px-3 py-2 bg-[var(--surface-color)] border border-dashed border-gray-500 text-gray-500 rounded-lg">
+    Hinzufügen
+  </button>
+</template>
