57489c364fd158dda683f41aeeeb5d717e56fff4
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-06-03 21:38:15 +0200

    Update EducationModul add emits

--- Project/Frontend/createYourself/src/components/ui/editor/EducationModul.vue
+++ Project/Frontend/createYourself/src/components/ui/editor/EducationModul.vue
@@ -1,12 +1,37 @@
 <script lang="ts" setup>
 
 import ModulStruct from "@/components/ui/editor/ModulStruct.vue";
+import AddButton from "@/components/ui/editor/AddButton.vue";
+
+const emit = defineEmits<{
+  (e: 'add') : void,
+  (e: 'delete'): void,
+  (e: 'up'): void,
+  (e: 'down'): void,
+}>()
+
+function onAdd() {
+  emit('add');
+}
+
+function onDelete(){
+  emit('delete')
+}
+
+function onUp(){
+  emit('up')
+}
+
+function onDown(){
+  emit('down')
+}
 </script>
 
 <template>
-  <ModulStruct type="Ausbildung" name="3 Einträge" svg="fa-solid fa-graduation-cap">
+  <ModulStruct @delete="onDelete()" @up="onUp()" @down="onDown()" type="Ausbildung" name="3 Einträge" svg="fa-solid fa-graduation-cap">
     <div class="flex flex-col gap-2">
       <slot></slot>
+      <AddButton @click="onAdd()"></AddButton>
     </div>
   </ModulStruct>
 </template>
