74500382edcd30a65659ca1c6c565ee29bd6165f
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-06-03 21:37:47 +0200

    Update ContentText add emit

--- Project/Frontend/createYourself/src/components/ui/editor/ContentText.vue
+++ Project/Frontend/createYourself/src/components/ui/editor/ContentText.vue
@@ -3,16 +3,34 @@ import {onMounted, ref, watch} from "vue";
   import InputStruct from "@/components/ui/editor/InputStruct.vue";
   import type {TextBlockContent} from "@/types/textBlockContent.ts";
   import ContentStruct from "@/components/ui/editor/ContentStruct.vue";
+import {parse} from "vite";
 
   const props = defineProps<{
     textBlock: TextBlockContent,
   }>()
 
+  const emit = defineEmits<{
+    (e: 'update', updated: TextBlockContent) : void,
+  }>()
+
+  function onUpdate() {
+    const updatedTextBlock : TextBlockContent = {
+      id: props.textBlock.id,
+      text: inputText.value,
+      tag: inputTag.value,
+      align: inputAlign.value,
+      fontSize: inputSize.value,
+      fontWeight: inputWeight.value,
+    }
+
+    emit('update', updatedTextBlock)
+  }
+
   const inputText = ref<string>(props.textBlock.text)
-  const inputTag = ref<string>(props.textBlock.tag ?? 'p')
-  const inputAlign = ref<string>(props.textBlock.align ?? 'left')
+  const inputTag = ref<NonNullable<TextBlockContent['tag']>>(props.textBlock.tag ?? 'p')
+  const inputAlign = ref<NonNullable<TextBlockContent['align']>>(props.textBlock.align ?? 'left')
   const inputSize = ref<number>(props.textBlock.fontSize ?? 12)
-  const inputWeight = ref<string>(props.textBlock.fontWeight ?? 'normal')
+  const inputWeight = ref<NonNullable<TextBlockContent['fontWeight']>>(props.textBlock.fontWeight ?? 'normal')
 
   watch(() => props.textBlock, (newTextBlock) => {
     inputText.value = newTextBlock.text;
@@ -41,19 +59,15 @@ import {onMounted, ref, watch} from "vue";
   <ContentStruct>
     <span class="md-subtitle text-[var(--text-color-light)]">Inhalt</span>
 
-<!--    <InputStruct title="Text">-->
-<!--      <input v-model="text" class="default-input w-full" type="text"/>-->
-<!--    </InputStruct>-->
-
     <div>
       <InputStruct title="Text">
-        <textarea v-model="inputText" class="default-input w-full"/>
+        <textarea @change="onUpdate()" v-model="inputText" class="default-input w-full"/>
       </InputStruct>
       <span class="text-[var(--text-color-light)] text-sm font-light">108 / 500 Zeichen</span>
     </div>
 
     <InputStruct title="Tag">
-      <select v-model="inputTag" class="default-input w-full">
+      <select @change="onUpdate()" v-model="inputTag" class="default-input w-full">
         <option value="p">Paragraph</option>
         <option value="h1">Titel</option>
         <option value="h2">Untertitel</option>
@@ -62,13 +76,13 @@ import {onMounted, ref, watch} from "vue";
     </InputStruct>
 
     <InputStruct title="Grösse">
-      <select v-model="inputSize" class="default-input w-full">
+      <select @change="onUpdate()" v-model="inputSize" class="default-input w-full">
         <option v-for="size in sizes" :value="size">{{ size }}</option>
       </select>
     </InputStruct>
 
     <InputStruct title="Weight">
-      <select v-model="inputWeight" class="default-input w-full">
+      <select @change="onUpdate()" v-model="inputWeight" class="default-input w-full">
         <option value="normal">Normal</option>
         <option value="medium">Medium</option>
         <option value="semibold">Semibold</option>
@@ -76,16 +90,14 @@ import {onMounted, ref, watch} from "vue";
       </select>
     </InputStruct>
 
-    <InputStruct title="Stil">
-      <select class="default-input w-full cursor-pointer outline-none">
-        <option>Überschrift + Unterzeile</option>
-        <option>Beispiel 1</option>
-        <option>Beispiel 2</option>
-      </select>
-    </InputStruct>
+<!--    <InputStruct title="Stil">-->
+<!--      <select class="default-input w-full cursor-pointer outline-none">-->
+<!--        <option>Überschrift + Unterzeile</option>-->
+<!--      </select>-->
+<!--    </InputStruct>-->
 
     <InputStruct title="Ausrichtung">
-      <select v-model="inputAlign" class="default-input w-full cursor-pointer outline-none">
+      <select @change="onUpdate()" v-model="inputAlign" class="default-input w-full cursor-pointer outline-none">
         <option value="left">Links</option>
         <option value="center">Zentriert</option>
         <option value="right">Rechts</option>
