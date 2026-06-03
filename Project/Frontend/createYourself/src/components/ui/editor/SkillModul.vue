a322d58354bd22429f94872692179b13bf645d07
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-06-03 21:39:55 +0200

    Update SkillModul add emits

--- Project/Frontend/createYourself/src/components/ui/editor/SkillModul.vue
+++ Project/Frontend/createYourself/src/components/ui/editor/SkillModul.vue
@@ -1,11 +1,36 @@
 <script lang="ts" setup>
   import ModulStruct from "@/components/ui/editor/ModulStruct.vue";
+  import AddButton from "@/components/ui/editor/AddButton.vue";
+
+  const emit = defineEmits<{
+    (e: 'add'): void,
+    (e: 'delete'): void,
+    (e: 'up'): void,
+    (e: 'down'): void,
+  }>()
+
+  function onAdd() {
+    emit('add')
+  }
+
+  function onDelete() {
+    emit('delete')
+  }
+
+  function up(){
+    emit('up')
+  }
+
+  function down(){
+    emit('down')
+  }
 </script>
 
 <template>
-  <ModulStruct type="Skills" name="Design" svg="fa-regular fa-star">
+  <ModulStruct @delete="onDelete()" @up="up()" @down="down()" type="Skills" name="Design" svg="fa-regular fa-star">
     <div class="grid grid-cols-2 gap-x-10 gap-y-2">
       <slot class="grid-cols-1"></slot>
+      <AddButton @click="onAdd()"></AddButton>
     </div>
   </ModulStruct>
 </template>
