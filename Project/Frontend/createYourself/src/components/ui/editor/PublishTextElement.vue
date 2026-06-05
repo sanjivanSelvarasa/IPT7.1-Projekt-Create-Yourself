44bb8386a666c40bedc81957d20c1f011d828b55
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-06-06 00:00:55 +0200

    Create PublishTextElement.vue

---
+++ Project/Frontend/createYourself/src/components/ui/editor/PublishTextElement.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+  import type {TextBlockContent} from "@/types/textBlockContent.ts";
+
+  const props = defineProps<{
+    textBlock: TextBlockContent,
+  }>()
+</script>
+
+<template>
+  <span :style="{color: props.textBlock.color, textAlign: props.textBlock.align, fontSize: props.textBlock.fontSize, fontWeight: props.textBlock.fontWeight}">{{ props.textBlock.text }}</span>
+</template>
