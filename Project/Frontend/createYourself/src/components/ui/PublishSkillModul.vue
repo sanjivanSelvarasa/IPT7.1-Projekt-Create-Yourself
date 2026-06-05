f93fcee30c2e656ae7ee50c7a7b1184804b0d397
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-06-06 00:01:16 +0200

    Create PublishSkillModul.vue

---
+++ Project/Frontend/createYourself/src/components/ui/PublishSkillModul.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="w-full h-fit min-h-[50px] px-10 py-9 shadow-md rounded-xl border border-gray-200 bg-[var(--surface-color)]">
+    <slot></slot>
+  </div>
+</template>
