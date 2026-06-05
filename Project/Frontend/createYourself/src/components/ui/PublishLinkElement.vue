316e9b9f610545b48fa6b3a50c05b435b8b9129c
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-06-06 00:01:01 +0200

    Create PublishLinkElement.vue

---
+++ Project/Frontend/createYourself/src/components/ui/PublishLinkElement.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+  import SvgStruct from "@/components/ui/SvgStruct.vue";
+
+  const props = defineProps<{
+    svg: string,
+    name: string,
+    url: string,
+  }>()
+</script>
+
+<template>
+  <button class="hover:shadow-md hover:-translate-y-1 transition duration-150 shadow-none px-4 py-3 border border-gray-200 bg-gray-50 rounded-lg text-sm font-semibold">
+    <a :href="props.url" target="_blank" class="flex items-center justify-center gap-3">
+      <SvgStruct class="text-[var(--primary-color)]">
+        <i :class="props.svg"></i>
+      </SvgStruct>
+      {{ props.name }}
+    </a>
+  </button>
+</template>
