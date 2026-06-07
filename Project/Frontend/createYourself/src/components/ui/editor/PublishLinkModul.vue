f7239b1b90a2d6bfd782ea5171b8215389bad70f
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-06-08 00:44:36 +0200

    Update PublishLinkModul fix responsive bug

--- Project/Frontend/createYourself/src/components/ui/editor/PublishLinkModul.vue
+++ Project/Frontend/createYourself/src/components/ui/editor/PublishLinkModul.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div class="w-full min-h-[50px] shadow-md bg-[var(--surface-color)] flex flex-col justify-center items-center gap-5 px-10 py-9 rounded-2xl border border-gray-200">
-    <h1 class="text-[35px]!">Interessiert an einer Zusammenarbeit?</h1>
+    <h1 class="text-[35px]! text-center">Interessiert an einer Zusammenarbeit?</h1>
     <span class="text-[var(--text-color-light)] max-w-[500px] text-center">Ich freue mich über neue Projekte, spannende Ideen oder einfach einen netten Austausch.</span>
 
     <div>
