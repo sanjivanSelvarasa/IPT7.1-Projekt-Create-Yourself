7ed4a2bc1d57990bec486a21321c1aa7a2122730
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-06-08 00:47:53 +0200

    Update Public vue add responsiveness & hero section logic & add footer

--- Project/Frontend/createYourself/src/pages/Public.vue
+++ Project/Frontend/createYourself/src/pages/Public.vue
@@ -22,6 +22,7 @@
   import PublishLinkModul from "@/components/ui/editor/PublishLinkModul.vue";
   import PublishLinkElement from "@/components/ui/PublishLinkElement.vue";
   import PublishTextElement from "@/components/ui/editor/PublishTextElement.vue";
+  import {getBrandSvg} from "@/utils/brand.ts";
 
   const route = useRoute();
   const portfolioSlug = String(route.params.slug);
@@ -121,12 +122,24 @@
       await portfolioSectionStore.getSections(portfolioFacts.value?.id, portfolioFacts.value?.currentVersionId)
       await loadSortedSections()
     }
-
-    console.log(sortedSections.value)
   })
 
-  const firstname = ref<string>('Egor')
-  const lastname = ref<string>('Biriukov')
+  const rawName = computed(() => {
+    return sortedSections.value?.find(s => s.sectionType === 'Hero Section').editorBlock[1].textBlockContent.text
+  })
+
+  const firstname = computed(() => {
+    if(rawName.value === null || rawName.value === undefined) return
+
+    return rawName.value.split(' ')[0]
+  })
+
+  const lastname = computed(() => {
+    if(rawName.value === null || rawName.value === undefined) return
+
+    return rawName.value.split(' ')[1]
+  })
+
 
   const mapForTabs = computed(() => {
     if(sortedSections?.value === null) return;
@@ -137,94 +150,71 @@
     }))
   })
 
-  const brands = [
-    {
-      name: 'youtube',
-      icon: 'fa-brands fa-youtube',
-    },
-    {
-      name: 'email',
-      icon: 'fa-regular fa-envelope',
-    },
-    {
-      name: 'github',
-      icon: 'fa-brands fa-github',
-    },
-    {
-      name: 'linkedin',
-      icon: 'fa-brands fa-linkedin',
-    },
-    {
-      name: 'twitter',
-      icon: 'fa-brands fa-x-twitter',
-    },
-    {
-      name: 'instagram',
-      icon: 'fa-brands fa-instagram',
-    },
-    {
-      name: 'tiktok',
-      icon: 'fa-brands fa-tiktok',
-    },
-  ]
-  function getBrandSvg(text: string) {
-    for (const brand of brands) {
-      if(text.trim().toLowerCase().includes(brand.name.toLowerCase()))
-        return brand.icon;
-    }
+  const projectSectionId = computed(() => {
+    return sortedSections.value?.find(s => s.sectionType === 'Projekte')?.id ?? null
+  })
 
-    return 'fa-solid fa-globe'
-  }
+  const contactSectionId = computed(() => {
+    return sortedSections.value?.find(s => s.sectionType === 'Kontakt & Social')?.id ?? null
+  })
+
+  const footerLinks = computed(() => {
+    if(socialLinkStore.socialLinks === null) return
+    return socialLinkStore.socialLinks?.slice(0, 3)
+  })
 </script>
 
 <template>
-  <div class="w-full h-full bg-[var(--background-color)] max-w-[100vw] overflow-x-scroll">
+  <div class="w-full h-full bg-[var(--background-color)] max-w-[100vw] overflow-x-hidden">
     <div class="max-w-[1200px] mx-auto mb-5 px-4">
       <NavPublic :firstname="firstname" :lastname="lastname" :tabs="mapForTabs"></NavPublic>
 
       <main>
         <section v-for="section in sortedSections" :key="section.id" :id="`${section.id}`">
           <header v-if="section.sectionType === 'Hero Section' " class="h-[100vh] w-full flex justify-center items-center">
-            <div class="max-w-[1200px] w-full">
-              <div class="flex justify-between items-center gap-4">
+            <div class="max-w-[1200px] w-full flex flex-col items-center lg:block">
+              <div class="flex lg:flex-row flex-col-reverse mt-30 lg:mt-0 justify-between items-center gap-4">
                 <div class="flex flex-col justify-center items-start gap-4">
                   <div class="flex flex-col gap-1 text-[70px]! font-medium!">
-                    <h1 class="text-[80px]! font-medium!">Hallo, ich bin</h1>
-                    <h1 class="text-[80px]! font-medium!">{{ firstname + ' ' + lastname }}.</h1>
+                    <PublishTextElement class="leading-tight" :text-block="section.editorBlock[0].textBlockContent"></PublishTextElement>
+                    <PublishTextElement class="leading-tight"  :text-block="section.editorBlock[1].textBlockContent"></PublishTextElement>
                   </div>
-                  <h2 class="bg-linear-to-r from-[var(--primary-color)] to-[var(--secondary-color)] bg-clip-text text-transparent text-2xl!">Frontend Developer & UI Designer</h2>
-                  <span class="text-lg text-[var(--text-color-light)] max-w-[450px]">Ich entwickle moderne, benutzerfreundliche Websiten und digitale Applikationen mit Fokus auf sauberes Design und durchdachte Nutzererlebnisse.</span>
+                  <PublishTextElement class="bg-linear-to-r! from-[var(--primary-color)]! to-[var(--secondary-color)]! bg-clip-text! text-transparent!" :text-block="section.editorBlock[2].textBlockContent"></PublishTextElement>
+                  <PublishTextElement class="max-w-[450px]" :text-block="section.editorBlock[3].textBlockContent"></PublishTextElement>
 
                   <div class="flex gap-3">
-                    <button class="select-none hover:text-[var(--primary-color)] hover:border-[var(--primary-color)] hover:bg-[var(--surface-color)] transition duration-75 border border-transparent flex justify-center items-center gap-2 px-4 py-3 rounded-xl shadow-md bg-[var(--primary-color)] text-[var(--text-color-white)]">
+                    <a :href="`#${projectSectionId}`" class="select-none hover:text-[var(--primary-color)] hover:border-[var(--primary-color)] hover:bg-[var(--surface-color)] transition duration-75 border border-transparent flex justify-center items-center gap-2 px-4 py-3 rounded-xl shadow-md bg-[var(--primary-color)] text-[var(--text-color-white)]">
                       <span>Projekte ansehen</span>
                       <SvgStruct>
                         <i class="fa-solid fa-arrow-right"></i>
                       </SvgStruct>
-                    </button>
+                    </a>
 
-                    <button class="select-none hover:text-[var(--primary-color)] hover:border-[var(--primary-color)] transition duration-75 flex justify-center items-center gap-2 px-4 py-3 rounded-xl shadow-sm text-[var(--text-color)] bg-[var(--surface-color)] border border-gray-200">
+                    <a :href="`#${contactSectionId}`" class="select-none hover:text-[var(--primary-color)] hover:border-[var(--primary-color)] transition duration-75 flex justify-center items-center gap-2 px-4 py-3 rounded-xl shadow-sm text-[var(--text-color)] bg-[var(--surface-color)] border border-gray-200">
                       <SvgStruct>
                         <i class="fa-regular fa-envelope"></i>
                       </SvgStruct>
                       <span>Kontakt aufnehmen</span>
-                    </button>
+                    </a>
                   </div>
                 </div>
 
                 <div>
-                  <div class="select-none relative flex justify-center text-[80px] text-[var(--text-color-white)] font-extrabold items-center w-[400px] h-[400px] rounded-full shadow-2xl bg-linear-to-br from-[var(--primary-color)] to-[var(--secondary-color)] border-5 border-[var(--surface-color)]">
-                    <div class="rotate -z-1 absolute box-content! top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] w-full h-full p-3 bg-linear-to-br from-[var(--primary-color)] to-[var(--secondary-color)] rounded-full"></div>
-                    <span>{{ firstname.slice(0,1) + lastname.slice(0,1) }}</span>
+                  <div class="relative select-none w-[400px] h-[400px]">
+                    <div class="rotate absolute -inset-2 bg-linear-to-br from-[var(--primary-color)] to-[var(--secondary-color)] rounded-full"></div>
+
+                    <div class="relative z-10 flex justify-center items-center w-full h-full text-[80px] text-[var(--text-color-white)] font-extrabold rounded-full shadow-2xl bg-linear-to-br from-[var(--primary-color)] to-[var(--secondary-color)] border-5 border-[var(--surface-color)]">
+                      <span>{{ firstname.slice(0,1) + lastname.slice(0,1) }}</span>
+                    </div>
                   </div>
                 </div>
               </div>
 
               <div class="mt-5 flex gap-3">
-                <button v-for="i in 3" :key="i" class="hover:text-[var(--primary-color)] hover:border-[var(--primary-color)] transition duration-75 min-w-[40px] min-h-[40px] rounded-xl shadow-sm border text-lg border-gray-200 text-gray-600">
-                  <a href="#" target="_blank">
+                <button v-for="link in section.editorBlock[section.editorBlock.length - 1].link" :key="link.id" class="hover:text-[var(--primary-color)] hover:border-[var(--primary-color)] transition duration-75 min-w-[40px] min-h-[40px] rounded-xl shadow-sm border text-lg border-gray-200 text-gray-600">
+                  <a :href="link.url" target="_blank">
                     <SvgStruct>
-                      <i class="fa-brands fa-github"></i>
+                      <i :class="getBrandSvg(link.platform)"></i>
                     </SvgStruct>
                   </a>
                 </button>
@@ -232,17 +222,17 @@
             </div>
           </header>
 
-          <PublishModulStruct v-if="section.sectionType !== 'Hero Section' " :name="section.sectionType" :title="section.title" description="Eine Auswahl von Arbetien, an denen ich gearbeitet habe - von Web-Apps bis hin zu UI-Konzepten.">
+          <PublishModulStruct v-if="section.sectionType !== 'Hero Section' " :name="section.sectionType" :title="section.title" description="">
             <div v-for="block in section.editorBlock" :key="block.id" class="flex flex-col gap-6">
               <div class="py-5 px-1" v-if="block.blockType === 'text' ">
                 <PublishTextElement :text-block="block.textBlockContent"></PublishTextElement>
               </div>
 
-              <div class="grid grid-cols-3 gap-4">
-                <PublishProjectElement v-for="project in block.project" :key="project.id" :title="project.title" :description="project.description" :demo-url="project.demoUrl" :code-url="project.githubUrl" image=""></PublishProjectElement>
+              <div class="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+                <PublishProjectElement v-for="project in block.project" :key="project.id" :title="project.title" :description="project.description" :demo-url="project.demoUrl" :code-url="project.githubUrl" :image="project.imageUrl"></PublishProjectElement>
               </div>
 
-              <PublishSkillModul v-if="block.blockType === 'skill' " class="grid grid-cols-2 gap-x-10 gap-y-5">
+              <PublishSkillModul v-if="block.blockType === 'skill' " class="grid lg:grid-cols-2 grid-cols-1 gap-x-10 gap-y-5">
                   <PublishSkillElement v-for="skill in block.skills" :key="skill.id" :title="skill.name" :value="skill.level"></PublishSkillElement>
               </PublishSkillModul>
 
@@ -260,10 +250,52 @@
         </section>
       </main>
     </div>
+
+    <footer class="bg-[#0f172a] min-h-[80px] w-full text-[var(--text-color-white)] p-4">
+      <div class="max-w-[1200px] mx-auto">
+        <div class="flex items-center justify-between">
+          <a class="select-none font-semibold text-lg !bg-transparent">
+            {{ firstname }}<span class="bg-linear-to-r from-[var(--primary-color)] to-[var(--secondary-color)] text-transparent bg-clip-text">{{ lastname }}</span>
+          </a>
+          <div class="flex items-center justify-center gap-2">
+            <a
+              v-for="link in footerLinks"
+              :key="link.id"
+              :href="link.url"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="hover:bg-white/25 transition duration-75 flex items-center justify-center w-[35px] h-[35px] rounded-lg backdrop-blur-sm bg-backdrop"
+            >
+              <SvgStruct>
+                <i :class="getBrandSvg(link.platform)"></i>
+              </SvgStruct>
+            </a>
+          </div>
+        </div>
+
+        <div class="my-5 w-full h-[1px] bg-[var(--text-color-light)]"></div>
+
+        <div class="flex items-center justify-between text-[var(--text-color-light)] text-xs">
+          <span>© 2026 CreateYourself. Alle Rechte vorbehalten.</span>
+          <RouterLink to="/" class="cursor-pointer flex gap-2 items-center justify-center px-3 py-2 bg-logo rounded-full text-[var(--text-color-light)]!">
+            <img class="w-[15px] h-[15px] object-cover" src="../../public/CreateYourself_Logo.png" alt="CreateYourself Logo">
+            Erstellt mit CreateYourself
+          </RouterLink>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
 <style scoped>
+  .bg-logo{
+    background: rgba(249, 250, 251, 0.05);
+  }
+
+  .bg-backdrop{
+    background: rgba(249, 250, 251, 0.15);
+  }
+
   .rotate{
     animation: rotate 5s linear infinite;
   }
