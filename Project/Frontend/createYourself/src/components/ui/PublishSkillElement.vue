d9ff81b2d1adf42fcaaaf91218b8ec3d3890d0f9
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-06-08 00:46:10 +0200

    Update PublishSkillElement add responsive feature

--- Project/Frontend/createYourself/src/components/ui/PublishSkillElement.vue
+++ Project/Frontend/createYourself/src/components/ui/PublishSkillElement.vue
@@ -8,7 +8,7 @@
 <template>
   <div class="flex flex-col gap-2">
     <div class="flex justify-between items-center gap-4 w-full">
-      <span class="font-semibold">{{ props.title }}</span>
+      <span class="font-semibold">{{ props.title.slice(0, 35) }}</span>
       <span class="font-semibold text-[var(--primary-color)]">{{ props.value }}%</span>
     </div>
     <div class="relative w-full h-[10px] rounded-full bg-gray-100 overflow-hidden">
