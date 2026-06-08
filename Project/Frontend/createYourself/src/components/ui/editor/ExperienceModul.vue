abf57f9ecb3623c29bd8885d56c82074fc803165
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-06-08 14:22:37 +0200

    Create ExperienceModul.vue

---
+++ Project/Frontend/createYourself/src/components/ui/editor/ExperienceModul.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+
+import ModulStruct from "@/components/ui/editor/ModulStruct.vue";
+import AddButton from "@/components/ui/editor/AddButton.vue";
+
+const emit = defineEmits<{
+  (e: 'add') : void,
+  (e: 'delete'): void,
+  (e: 'up'): void,
+  (e: 'down'): void,
+}>()
+
+function onAdd() {
+  emit('add');
+}
+
+function onDelete(){
+  emit('delete')
+}
+
+function onUp(){
+  emit('up')
+}
+
+function onDown(){
+  emit('down')
+}
+</script>
+
+<template>
+  <ModulStruct @delete="onDelete()" @up="onUp()" @down="onDown()" type="Erfahrung" name="3 Einträge" svg="fa-solid fa-briefcase">
+    <div class="flex flex-col gap-2">
+      <slot></slot>
+      <AddButton @click="onAdd()"></AddButton>
+    </div>
+  </ModulStruct>
+</template>
