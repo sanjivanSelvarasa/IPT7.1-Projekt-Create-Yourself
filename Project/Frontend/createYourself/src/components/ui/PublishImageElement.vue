0e16c88d0fc5c4959758dba84d5706fe07927859
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-06-10 21:35:31 +0200

    Create PublishImageElement.vue

---
+++ Project/Frontend/createYourself/src/components/ui/PublishImageElement.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+  const props = defineProps<{
+    imageUrl?: string,
+    alt?: string,
+  }>()
+</script>
+
+<template>
+  <div class="px-4 py-4 shadow-md rounded-xl max-h-[350px] h-[350px] overflow-hidden border border-gray-200">
+    <img
+      v-if="props.imageUrl"
+      :src="`http://localhost:3000${imageUrl}`"
+      :alt="props.alt ?? 'Bild'"
+      class="w-full h-full object-cover rounded-xl"
+    />
+  </div>
+</template>
