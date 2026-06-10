a012a9bd1fc11d290e8730c2ee8d5a20fa2f2ae9
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-06-10 20:36:17 +0200

    Create PublishExperienceModul.vue

---
+++ Project/Frontend/createYourself/src/components/ui/PublishExperienceModul.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="w-full h-fit min-h-[50px] px-10 py-9 rounded-xl">
+    <div class="flex gap-10 justify-between items-stretch">
+      <div class="w-[3px] bg-linear-to-b from-[var(--primary-color)] to-[var(--secondary-color)] rounded-full mt-6"></div>
+      <div class="w-full gap-5 flex flex-col justify-between items-start">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
