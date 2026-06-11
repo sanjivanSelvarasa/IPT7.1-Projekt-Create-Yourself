a3fde25b68f56b9825e2b8fae39cd28ea7cb6bba
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-06-11 10:55:32 +0200

    Create section_types.ts

---
+++ Project/Frontend/createYourself/src/utils/section_types.ts
@@ -0,0 +1,8 @@
+export const SECTION_TYPES = {
+  HERO: 'Hero Section',
+  PROJECTS: 'Projekte',
+  SKILLS: 'Skills',
+  EXPERIENCE: 'Erfahrung',
+  EDUCATION: 'Ausbildung',
+  CONTACT: 'Kontakt & Social',
+} as const
