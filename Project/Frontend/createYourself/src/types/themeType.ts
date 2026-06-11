1a761324f6bac215e8d41400fdb11c589f5c0863
sanjivanSelvarasa <sanjivan_selvarasa@sluz.ch>
2026-06-11 17:02:36 +0200

    Create themeType.ts

---
+++ Project/Frontend/createYourself/src/types/themeType.ts
@@ -0,0 +1,11 @@
+export type ThemeType = {
+  id: number;
+  name: string;
+  primaryColor: string;
+  secondaryColor: string;
+  accentColor: string;
+  backgroundColor: string;
+  surfaceColor: string;
+  textColor: string;
+  createdAt?: string;
+};
